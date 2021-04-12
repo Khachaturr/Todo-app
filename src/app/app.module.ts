3e6c4eec90622c67505e13a79a542f50e8dd10e1
Khachaturr <grantovich91@mail.ru>
2021-04-12 10:44:56 +0400

    registration and todos component for todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule
