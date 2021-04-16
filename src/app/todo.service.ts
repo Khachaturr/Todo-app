d2044e85b874a321c214919b76f7603be7bdd9f3
Khachaturr <grantovich91@mail.ru>
2021-04-16 13:21:57 +0400

    add functional  for adding new todos to list and add functional to delete todo and add ability to edit single todo

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,19 +1,54 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  taskObservable=new Subject();
+  tasksOvserable= new Subject();
+  remuveTaskObservable= new Subject();
+  editTaskOvservable=new Subject();
 
 
 
   constructor(private http:HttpClient) { }
   sendTaskServer(body){
     this.http.post('http://localhost:9000/todo/tasks', body).subscribe((res)=>{
-      // console.log(res)
+      // console.log(res['valid'])
+      if(res['valid']==='true'){
+        // console.log("gh")
+        this.taskObservable.next(body)
+      }
     })
+  }
+  getTasksIsServer(){
+    this.http.get("http://localhost:9000/todo/all/tasks").subscribe((res)=>{ 
+      this.tasksOvserable.next(res)
+    })
+    
+
+  }
+  delateTask(id){
+    this.http.delete(`http://localhost:9000/todo/task/delete/${id}`).subscribe((res)=>{
+      
+      if(res['valid']==='true'){
+        // console.log(res['valid'])
+        this.remuveTaskObservable.next(id)
+
+      }
+    }, (error)=>{alert(error.error)})
+
+  }
+  editTask(body){
+    this.http.put('http://localhost:9000/todo/task/edit',body).subscribe((res)=>{
+      // console.log(res)
+      if(res['valid']==='true'){
+        this.editTaskOvservable.next(body)
+      }
+    },(error)=>alert(error.error))
   }
   
 
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,15 +1,21 @@
 import { TodoService } from './../todo.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Subscriber, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent implements OnInit, OnDestroy {
   taskFormGroup:FormGroup;
+  taskSubscription:Subscription;
+  tasksSubscription:Subscription;
+  remuveTaskSubscription:Subscription;
+  editTaskSubscription:Subscription;
+  taskId:number;
   tasksArr = []
   
 
@@ -21,24 +27,60 @@ export class TodosComponent implements OnInit {
     this.taskFormGroup=this.formbuilder.group({
       task:['', Validators.required]
     })
-    this.taskFormGroup.valueChanges.subscribe((res)=>{console.log(res)})
-    this.http.get("http://localhost:9000/todo/all/tasks").subscribe((res)=>{
+    this.todoService.getTasksIsServer();
+    this.tasksSubscription=this.todoService.tasksOvserable.subscribe((res)=>{
+      // console.log(res)
       let arr=JSON.parse(res['task'])
-      
-      console.log(arr)
       this.tasksArr=[...arr]
-      console.log(this.tasksArr)
+    })
+    this.remuveTaskSubscription=this.todoService.remuveTaskObservable.subscribe((res)=>{
+      let index=this.tasksArr.findIndex((task)=>task.id==res)
+      this.tasksArr.splice(index,1)
+    })
+
+    this.taskSubscription=this.todoService.taskObservable.subscribe((res)=>{this.tasksArr.push(res)})
+    this.editTaskSubscription=this.todoService.editTaskOvservable.subscribe((res)=>{
+      let index=this.tasksArr.findIndex((task)=>task.id==res['id'])
+      this.tasksArr.splice(index, 1, res)
+      this.taskFormGroup.reset()
+      // console.log(index)
     })
 
   }
+  ngOnDestroy(){
+    this.taskSubscription.unsubscribe;
+    this.tasksSubscription.unsubscribe;
+    this.editTaskSubscription.unsubscribe;
+  }
   addTask(){
+    
+    if(this.taskId){
+      // console.log(this.taskFormGroup.value.task)
+      let body={task:this.taskFormGroup.value.task,
+                id:this.taskId}
+      this.todoService.editTask(body)
+
+    }else{
     let id=Math.random();
     let body={task:this.taskFormGroup.value.task,
               id:id};
+              
     this.todoService.sendTaskServer(body);
     this.taskFormGroup.reset()
-    console.log(body)
+    // console.log(body)
+    }
+  }
+
+  remuve(id){
+        this.todoService.delateTask(id)
+  }
+
+  editTask(task){
+    this.taskFormGroup.controls.task.setValue(task.task)
+    this.taskId=task.id
     
+    // console.log(this.taskFormGroup)
+
   }
 
 }
