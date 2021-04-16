54e92372a53eb597ced82516a6f128d1e6275d11
Khachaturr <grantovich91@mail.ru>
2021-04-16 16:56:51 +0400

    add functional to mark as done

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -7,49 +7,59 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class TodoService {
-  taskObservable=new Subject();
-  tasksOvserable= new Subject();
-  remuveTaskObservable= new Subject();
-  editTaskOvservable=new Subject();
+  taskObservable = new Subject();
+  tasksOvserable = new Subject();
+  remuveTaskObservable = new Subject();
+  editTaskOvservable = new Subject();
+  taskDoneOvservable= new Subject();
 
 
 
-  constructor(private http:HttpClient) { }
-  sendTaskServer(body){
-    this.http.post('http://localhost:9000/todo/tasks', body).subscribe((res)=>{
+  constructor(private http: HttpClient) { }
+  sendTaskServer(body) {
+    this.http.post('http://localhost:9000/todo/tasks', body).subscribe((res) => {
       // console.log(res['valid'])
-      if(res['valid']==='true'){
+      if (res['valid'] === 'true') {
         // console.log("gh")
         this.taskObservable.next(body)
       }
     })
   }
-  getTasksIsServer(){
-    this.http.get("http://localhost:9000/todo/all/tasks").subscribe((res)=>{ 
+  getTasksIsServer() {
+    this.http.get("http://localhost:9000/todo/all/tasks").subscribe((res) => {
       this.tasksOvserable.next(res)
     })
-    
+
 
   }
-  delateTask(id){
-    this.http.delete(`http://localhost:9000/todo/task/delete/${id}`).subscribe((res)=>{
-      
-      if(res['valid']==='true'){
+  delateTask(id) {
+    this.http.delete(`http://localhost:9000/todo/task/delete/${id}`).subscribe((res) => {
+
+      if (res['valid'] === 'true') {
         // console.log(res['valid'])
         this.remuveTaskObservable.next(id)
 
       }
-    }, (error)=>{alert(error.error)})
+    }, (error) => { alert(error.error) })
 
   }
-  editTask(body){
-    this.http.put('http://localhost:9000/todo/task/edit',body).subscribe((res)=>{
+  editTask(body) {
+    this.http.put('http://localhost:9000/todo/task/edit', body).subscribe((res) => {
       // console.log(res)
-      if(res['valid']==='true'){
+      if (res['valid'] === 'true') {
         this.editTaskOvservable.next(body)
       }
-    },(error)=>alert(error.error))
+    }, (error) => alert(error.error))
   }
-  
+  taskDone(body) {
+     this.http.patch('http://localhost:9000/todo/edit/task/done',body).subscribe((res)=>{
+       
+       if(res['valid']==='true'){
+        console.log(res)
+         this.taskDoneOvservable.next(body)
+       }
+     })
+  }
+
 
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,7 +2,7 @@ import { TodoService } from './../todo.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { Subscriber, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
@@ -10,76 +10,105 @@ import { Subscriber, Subscription } from 'rxjs';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit, OnDestroy {
-  taskFormGroup:FormGroup;
-  taskSubscription:Subscription;
-  tasksSubscription:Subscription;
-  remuveTaskSubscription:Subscription;
-  editTaskSubscription:Subscription;
-  taskId:number;
+  taskFormGroup: FormGroup;
+  taskSubscription: Subscription;
+  tasksSubscription: Subscription;
+  remuveTaskSubscription: Subscription;
+  editTaskSubscription: Subscription;
+  taskDoneSubscription: Subscription;
+  taskId: number;
   tasksArr = []
-  
 
-  constructor(private todoService:TodoService,
-              private formbuilder:FormBuilder,
-              private http:HttpClient) { }
+
+
+  constructor(private todoService: TodoService,
+    private formbuilder: FormBuilder,
+    private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.taskFormGroup=this.formbuilder.group({
-      task:['', Validators.required]
+    this.taskFormGroup = this.formbuilder.group({
+      task: ['', Validators.required]
     })
     this.todoService.getTasksIsServer();
-    this.tasksSubscription=this.todoService.tasksOvserable.subscribe((res)=>{
+    this.tasksSubscription = this.todoService.tasksOvserable.subscribe((res) => {
       // console.log(res)
-      let arr=JSON.parse(res['task'])
-      this.tasksArr=[...arr]
+      let arr = JSON.parse(res['task'])
+      arr.sort((a,b)=>{return a.checked-b.checked})
+      
+      this.tasksArr = [...arr]
+      console.log(this.tasksArr)
+
     })
-    this.remuveTaskSubscription=this.todoService.remuveTaskObservable.subscribe((res)=>{
-      let index=this.tasksArr.findIndex((task)=>task.id==res)
-      this.tasksArr.splice(index,1)
+    this.remuveTaskSubscription = this.todoService.remuveTaskObservable.subscribe((res) => {
+      let index = this.tasksArr.findIndex((task) => task.id == res)
+      this.tasksArr.splice(index, 1)
     })
 
-    this.taskSubscription=this.todoService.taskObservable.subscribe((res)=>{this.tasksArr.push(res)})
-    this.editTaskSubscription=this.todoService.editTaskOvservable.subscribe((res)=>{
-      let index=this.tasksArr.findIndex((task)=>task.id==res['id'])
+    this.taskSubscription = this.todoService.taskObservable.subscribe((res) => { 
+      this.tasksArr.push(res)
+      this.tasksArr.sort((a,b)=>{return a.checked-b.checked})})
+    this.editTaskSubscription = this.todoService.editTaskOvservable.subscribe((res) => {
+      let index = this.tasksArr.findIndex((task) => task.id == res['id'])
       this.tasksArr.splice(index, 1, res)
       this.taskFormGroup.reset()
       // console.log(index)
     })
+    this.taskDoneSubscription = this.todoService.taskDoneOvservable.subscribe((res) => {
+      let task = this.tasksArr.find((task) => task.id === res["id"])
+      task.checked = res["checked"]
+      this.tasksArr.sort((a,b)=>{return a.checked-b.checked})
+      console.log(task)
+    })
 
   }
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.taskSubscription.unsubscribe;
     this.tasksSubscription.unsubscribe;
     this.editTaskSubscription.unsubscribe;
   }
-  addTask(){
-    
-    if(this.taskId){
+  addTask() {
+
+    if (this.taskId) {
       // console.log(this.taskFormGroup.value.task)
-      let body={task:this.taskFormGroup.value.task,
-                id:this.taskId}
+     
+      let body = {
+        task: this.taskFormGroup.value.task,
+        id: this.taskId,
+        checked: false
+      }
       this.todoService.editTask(body)
 
-    }else{
-    let id=Math.random();
-    let body={task:this.taskFormGroup.value.task,
-              id:id};
-              
-    this.todoService.sendTaskServer(body);
-    this.taskFormGroup.reset()
-    // console.log(body)
+    } else {
+    
+      let id = Math.random();
+      let body = {
+        task: this.taskFormGroup.value.task,
+        id: id,
+        checked: false
+      };
+
+      this.todoService.sendTaskServer(body);
+      this.taskFormGroup.reset()
+      // console.log(body)
     }
   }
 
-  remuve(id){
-        this.todoService.delateTask(id)
+  remuve(id) {
+    this.todoService.delateTask(id)
   }
 
-  editTask(task){
+  editTask(task) {
     this.taskFormGroup.controls.task.setValue(task.task)
-    this.taskId=task.id
-    
-    // console.log(this.taskFormGroup)
+    this.taskId = task.id
+  }
+
+  taskDone(event, id) {
+
+    console.log(event.target.checked, id)
+    this.todoService.taskDone({
+      checked: event.target.checked,
+      id: id
+    })
 
   }
 
