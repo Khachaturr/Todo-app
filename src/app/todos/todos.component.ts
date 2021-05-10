3f2db1fcbfe5010c2373ade4e92e0deee8779bce
Khachaturr <grantovich91@gmail.com>
2021-05-10 15:09:34 +0400

    implement show/hide "done" tasks, add delete confirmation

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,100 +1,130 @@
 import { TodoService } from './../todo.service';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ValidationService } from '../Validation.service';
+
+
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent implements OnInit, OnDestroy {
+export class TodosComponent implements OnInit {
+  name: string;
+  sureName: string;
+  _errorMessage: string
+  taskId: number = null;
   taskFormGroup: FormGroup;
-  taskSubscription: Subscription;
-  tasksSubscription: Subscription;
-  remuveTaskSubscription: Subscription;
-  editTaskSubscription: Subscription;
-  taskDoneSubscription: Subscription;
-  taskId: number;
+  confirmDeleteSection: boolean = false;
+  isdeclaretionTaskInput: boolean = false;
+  classList: any;
+  isshownDoneTasks: boolean;
   tasksArr = []
+  hideChecked = false
+  editTaskBody: {};
+  addTaskBody: {};
 
 
 
-  constructor(private todoService: TodoService,
-    private formbuilder: FormBuilder,
-    private http: HttpClient) { }
+  constructor(private todoService: TodoService,) {
+
+  }
 
   ngOnInit(): void {
-    this.taskFormGroup = this.formbuilder.group({
-      task: ['', Validators.required]
-    })
-    this.todoService.getTasksIsServer();
-    this.tasksSubscription = this.todoService.tasksOvserable.subscribe((res) => {
-      // console.log(res)
+    this.getUserDataFromLocalStorage()
+    this.taskFormGroup = this.todoService.taskFormBuilder()
+    this.getTasksFromService()
+  }
+
+  getUserDataFromLocalStorage() {
+    let userInformStr = localStorage.getItem('UserInform');
+    let userInformObj = JSON.parse(userInformStr)
+    this.name = userInformObj.name;
+    this.sureName = userInformObj.sureName
+  }
+
+
+  getTasksFromService() {
+    this.todoService.getTasksFromServer().subscribe((res) => {
       let arr = JSON.parse(res['task'])
-      arr.sort((a,b)=>{return a.checked-b.checked})
-      
+      arr.sort((a, b) => { return a.checked - b.checked })
       this.tasksArr = [...arr]
-      console.log(this.tasksArr)
-
-    })
-    this.remuveTaskSubscription = this.todoService.remuveTaskObservable.subscribe((res) => {
-      let index = this.tasksArr.findIndex((task) => task.id == res)
-      this.tasksArr.splice(index, 1)
-    })
-
-    this.taskSubscription = this.todoService.taskObservable.subscribe((res) => { 
-      this.tasksArr.push(res)
-      this.tasksArr.sort((a,b)=>{return a.checked-b.checked})})
-    this.editTaskSubscription = this.todoService.editTaskOvservable.subscribe((res) => {
-      let index = this.tasksArr.findIndex((task) => task.id == res['id'])
-      this.tasksArr.splice(index, 1, res)
-      this.taskFormGroup.reset()
-      // console.log(index)
-    })
-    this.taskDoneSubscription = this.todoService.taskDoneOvservable.subscribe((res) => {
-      let task = this.tasksArr.find((task) => task.id === res["id"])
-      task.checked = res["checked"]
-      this.tasksArr.sort((a,b)=>{return a.checked-b.checked})
-      console.log(task)
-    })
-
+    }, (error) => alert(error.error))
   }
-  ngOnDestroy() {
-    this.taskSubscription.unsubscribe;
-    this.tasksSubscription.unsubscribe;
-    this.editTaskSubscription.unsubscribe;
-  }
-  addTask() {
 
-    if (this.taskId) {
-      // console.log(this.taskFormGroup.value.task)
-     
-      let body = {
-        task: this.taskFormGroup.value.task,
-        id: this.taskId,
-        checked: false
-      }
-      this.todoService.editTask(body)
-
-    } else {
-    
-      let id = Math.random();
-      let body = {
-        task: this.taskFormGroup.value.task,
-        id: id,
-        checked: false
-      };
-
-      this.todoService.sendTaskServer(body);
-      this.taskFormGroup.reset()
-      // console.log(body)
+  get errorMessage() {
+    for (let param in this.taskFormGroup.get('task').errors) {
+      return ValidationService.getValidatorErrorMessage(param)
     }
   }
 
-  remuve(id) {
-    this.todoService.delateTask(id)
+  creatEditTaskBody() {
+    this.editTaskBody = {
+      task: this.taskFormGroup.value.task,
+      id: this.taskId,
+      checked: false
+    }
+  }
+
+  creatAddTaskBody() {
+    let id = Math.random();
+    this.addTaskBody = {
+      task: this.taskFormGroup.value.task,
+      id: id,
+      checked: false
+    }
+  }
+
+  addTask() {
+    this.isdeclaretionTaskInput = true
+    if (this.taskId !== null && this.taskFormGroup.status !== "INVALID") {
+      this.creatEditTaskBody();
+
+      this.todoService.editTask(this.editTaskBody).subscribe((res) => {
+        let index = this.tasksArr.findIndex((task) => task.id == this.taskId)
+        this.tasksArr.splice(index, 1, this.editTaskBody)
+        this.taskFormGroup.reset()
+        this.taskId = null;
+      })
+
+      this.isdeclaretionTaskInput = false
+    } 
+    else if (this.taskFormGroup.status !== "INVALID") {
+      this.creatAddTaskBody()
+      
+      this.todoService.sendTaskServer(this.addTaskBody).subscribe((res) => {
+        this.tasksArr.push(this.addTaskBody)
+        this.tasksArr.sort((a, b) => { return a.checked - b.checked })
+      }, (error) => alert(error));
+
+      this.taskFormGroup.reset()
+      this.isdeclaretionTaskInput = false
+    }
+  }
+
+  toggleConfirmdeleteSection() {
+    this.confirmDeleteSection = !this.confirmDeleteSection
+    if (this.confirmDeleteSection === false) {
+      this.classList.remove('remuvIcone')
+    }
+
+  }
+
+  remuve(event, id) {
+    this.classList = event.target.classList
+    event.target.classList.add('remuvIcone')
+    this.toggleConfirmdeleteSection()
+    this.taskId = id
+  }
+
+  confirmRemuve() {
+    this.todoService.remuveTask(this.taskId).subscribe((res) => {
+      let index = this.tasksArr.findIndex((task) => task.id == this.taskId)
+      this.tasksArr.splice(index, 1);
+      this.taskId = null;
+    }, (error) => alert(error.error))
+    this.toggleConfirmdeleteSection()
   }
 
   editTask(task) {
@@ -103,13 +133,22 @@ export class TodosComponent implements OnInit, OnDestroy {
   }
 
   taskDone(event, id) {
-
-    console.log(event.target.checked, id)
     this.todoService.taskDone({
       checked: event.target.checked,
       id: id
-    })
+    }).subscribe((res) => {
+      let task = this.tasksArr.find((task) => task.id === id)
+      task.checked = event.target.checked
+      this.tasksArr.sort((a, b) => { return a.checked - b.checked })
+    }, (error) => alert(error.error))
+  }
 
+  logout() {
+    this.todoService.logout()
+  }
+
+  toggle() {
+    this.hideChecked = !this.hideChecked
   }
 
 }
